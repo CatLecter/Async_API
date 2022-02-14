23fd775c2630e4142286d8c5ed4983b7e4cf189a
TreOne <tre@tre.one>
2022-02-14 10:05:27 +0300

    Реализация backoff для подключения к elastic.

--- src/api/db/elastic.py
+++ src/api/db/elastic.py
@@ -1,8 +1,24 @@
+import logging
 from typing import Optional
 
+import backoff
 from elasticsearch import AsyncElasticsearch
 
+from core import config
+from core.utils import backoff_hdlr
+
+logger = logging.getLogger(__name__)
 es: Optional[AsyncElasticsearch] = None
+
+
+@backoff.on_exception(backoff.expo, ConnectionError, on_backoff=backoff_hdlr)
+async def elastic_connect():
+    global es
+    es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
+    result = await es.ping()
+    if not result:
+        raise ConnectionError('The ElasticSearch server is not responding.')
+    logger.info('Successfully connected to elasticsearch server.')
 
 
 # Функция понадобится при внедрении зависимостей
--- src/api/db/redis.py
+++ src/api/db/redis.py
@@ -8,10 +8,19 @@ from typing import Callable, Optional
 import aioredis.errors
 from aioredis import Redis
 
+from core import config
 from core.config import CACHE_TTL
 
 logger = logging.getLogger(__name__)
 redis: Optional[Redis] = None
+
+
+async def redis_connect():
+    global redis
+    redis = await aioredis.create_redis_pool(
+        (config.REDIS_HOST, config.REDIS_PORT), minsize=10, maxsize=20
+    )
+    logger.info('Successfully connected to redis server.')
 
 
 # Функция понадобится при внедрении зависимостей
@@ -27,14 +36,18 @@ def redis_cache_me(key_function: Callable, ttl: timedelta = CACHE_TTL):
         async def _wrapper(*args, **kwargs):
             cache_key = f'{func.__module__}:{func.__name__}:{key_function(*args, **kwargs)}'
 
+            pickled_data = None
+
             try:
                 pickled_data = await redis.get(cache_key)
-                if pickled_data is not None:
-                    data = pickle.loads(pickled_data)
-                    logger.info(f'Get data from Redis by key "{cache_key}".')
-                    return data
             except aioredis.errors.RedisError as e:
+                # Никаких backoff. Мы не будем ждать пока ответит кеш.
                 logger.error(e)
+
+            if pickled_data is not None:
+                data = pickle.loads(pickled_data)
+                logger.info(f'Get data from Redis by key "{cache_key}".')
+                return data
 
             data = await func(*args, **kwargs)
             logger.info(f'Put data to Redis by key "{cache_key}".')
