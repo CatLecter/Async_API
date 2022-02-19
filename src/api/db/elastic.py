2f934ef247c69569c8f82e65910312bc81479ba3
TreOne <tre@tre.one>
2022-02-19 19:06:17 +0300

    Рефакторинг подключения к базам.

--- src/api/db/elastic.py
+++ src/api/db/elastic.py
@@ -11,16 +11,18 @@ logger = logging.getLogger(__name__)
 es: Optional[AsyncElasticsearch] = None
 
 
+async def get_elastic() -> AsyncElasticsearch:
+    """Возвращает объект для асинхронного общения с сервисами ElasticSearch.
+    Функция понадобится при внедрении зависимостей."""
+    return es
+
+
 @backoff.on_exception(backoff.expo, ConnectionError, on_backoff=backoff_hdlr)
 async def elastic_connect():
+    """Устанавливает подключение к сервису ElasticSearch."""
     global es
     es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
     result = await es.ping()
     if not result:
         raise ConnectionError('The ElasticSearch server is not responding.')
     logger.info('Successfully connected to elasticsearch server.')
-
-
-# Функция понадобится при внедрении зависимостей
-async def get_elastic() -> AsyncElasticsearch:
-    return es
--- src/api/db/redis.py
+++ src/api/db/redis.py
@@ -1,62 +1,25 @@
-import asyncio
-import functools
 import logging
-import pickle
-from datetime import timedelta
-from typing import Callable, Optional
+from typing import Optional
 
 import aioredis.errors
 from aioredis import Redis
 
 from core import config
-from core.config import CACHE_TTL
 
 logger = logging.getLogger(__name__)
 redis: Optional[Redis] = None
 
 
+async def get_redis() -> Redis:
+    """Возвращает объект для асинхронного общения с сервисами Redis.
+    Функция понадобится при внедрении зависимостей."""
+    return redis
+
+
 async def redis_connect():
+    """Устанавливает подключение к сервису Redis."""
     global redis
     redis = await aioredis.create_redis_pool(
         (config.REDIS_HOST, config.REDIS_PORT), minsize=10, maxsize=20
     )
     logger.info('Successfully connected to redis server.')
-
-
-# Функция понадобится при внедрении зависимостей
-async def get_redis() -> Redis:
-    return redis
-
-
-def redis_cache_me(key_function: Callable, ttl: timedelta = CACHE_TTL):
-    """Декоратор для кэширования возвращаемого функцией значения в редис."""
-
-    def _decorator(func):
-        @functools.wraps(func)
-        async def _wrapper(*args, **kwargs):
-            cache_key = f'{func.__module__}:{func.__name__}:{key_function(*args, **kwargs)}'
-
-            pickled_data = None
-
-            try:
-                pickled_data = await redis.get(cache_key)
-            except aioredis.errors.RedisError as e:
-                # Никаких backoff. Мы не будем ждать пока ответит кеш.
-                logger.error(e)
-
-            if pickled_data is not None:
-                data = pickle.loads(pickled_data)
-                logger.info(f'Get data from Redis by key "{cache_key}".')
-                return data
-
-            data = await func(*args, **kwargs)
-            logger.info(f'Put data to Redis by key "{cache_key}".')
-            pickled_data = pickle.dumps(data)
-            asyncio.create_task(
-                redis.set(cache_key, pickled_data, expire=int(ttl.total_seconds()))
-            )
-            return data
-
-        return _wrapper
-
-    return _decorator
