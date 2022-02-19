3c947bbc23646024b09542e9cd60df2f66026a39
TreOne <tre@tre.one>
2022-02-19 21:32:53 +0300

    Правки в поисковом движке эластика, для большей гибкости.

--- src/api/engines/search/elastic.py
+++ src/api/engines/search/elastic.py
@@ -1,3 +1,5 @@
+from typing import Dict, Optional
+
 from elasticsearch import AsyncElasticsearch, NotFoundError
 from elasticsearch_dsl import Search
 from elasticsearch_dsl.query import MultiMatch, Nested, Term
@@ -13,16 +15,13 @@ class ElasticSearchEngine(SearchEngine):
     def __init__(self, service: AsyncElasticsearch):
         self.elastic = service
 
-    async def get_by_pk(self, table: str, pk: str) -> SearchResult:
+    async def get_by_pk(self, table: str, pk: str) -> Optional[Dict]:
         """Возвращает объект по ключу."""
-        result = SearchResult(items=[], total=0)
         try:
             doc = await self.elastic.get(index=table, id=pk)
         except NotFoundError:
-            return result
-        result.items = [doc['_source']]
-        result.total = 1
-        return result
+            return None
+        return doc['_source']
 
     async def search(self, table: str, params: SearchParams) -> SearchResult:
         """Возвращает объекты подходящие под параметры поиска."""
@@ -46,9 +45,10 @@ class ElasticSearchEngine(SearchEngine):
                         query=Term(**{f'{params.filter_field}__uuid': params.filter_value}),
                     )
                 )
-            start = (params.page_number - 1) * params.page_size
-            end = start + params.page_size
-            search = search[start:end]
+            if params.page_number and params.page_size:
+                start = (params.page_number - 1) * params.page_size
+                end = start + params.page_size
+                search = search[start:end]
             body = search.to_dict()
             docs = await self.elastic.search(index=table, body=body)
         except NotFoundError:
