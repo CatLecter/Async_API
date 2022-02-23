4f9031234a2da0fd27dbbe4abf2ac411c9b807a8
Vasilii Safronov <vasi1988@mail.ru>
2022-02-24 01:26:52 +0300

    Написаны тесты эндпоинтов фильмов/поиска по фильмам

--- tests/functional/src/test_film_search.py
+++ tests/functional/src/test_film_search.py
@@ -1,24 +1,56 @@
-# from http import HTTPStatus
-# from typing import Callable
-#
-# import pytest
-#
-#
-# @pytest.mark.usefixtures("data_setup")
-# @pytest.mark.data_setup_params("movies", "movies_list.json")
-# @pytest.mark.asyncio
-# class TestMoviesList:
-#     async def test_success(self, make_get_request: Callable):
-#         response = await make_get_request("/film/search", {"query": "дружно"})
-#
-#         assert response.status == HTTPStatus.OK
-#         assert len(response.body) == 2
-#
-#     async def test_check_query_and_paging(self, make_get_request: Callable):
-#         # всего 3 совпадения со словом "угроза"
-#         response = await make_get_request(
-#             "/film/search", {"query": "угроза", "page[number]": 2, "page[size]": 2}
-#         )
-#
-#         assert response.status == HTTPStatus.OK
-#         assert len(response.body) == 1
+import pytest
+from http import HTTPStatus
+
+
+@pytest.mark.asyncio
+async def test_films_search(
+    create_index,
+    make_get_request,
+    expected_json_response,
+):
+    response = await make_get_request("/films/search/", params={"query": "Star"})
+    assert response.status == HTTPStatus.OK
+    assert response.body == expected_json_response
+
+
+@pytest.mark.asyncio
+async def test_films_search_not_found(
+    create_index, make_get_request, expected_json_response
+):
+    response = await make_get_request("/films/search/", params={"query": "NotFound"})
+    assert response.status == HTTPStatus.OK
+    assert response.body == expected_json_response
+
+
+@pytest.mark.asyncio
+async def test_films_pagination(
+    create_index,
+    make_get_request,
+    expected_json_response,
+):
+    response = await make_get_request(
+        "/films/search/",
+        params={
+            "page[number]": 2,
+            "page[size]": 4
+        }
+    )
+    assert response.status == HTTPStatus.OK
+    assert response.body == expected_json_response
+
+
+@pytest.mark.asyncio
+async def test_films_all(
+    create_index,
+    make_get_request,
+    expected_json_response,
+):
+    response = await make_get_request(
+        "/films/search/",
+        params={
+            "page[number]": 1,
+            "page[size]": 10
+        }
+    )
+    assert response.status == HTTPStatus.OK
+    assert response.body == expected_json_response
